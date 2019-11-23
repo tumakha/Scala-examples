bd6190a56176cae9b3c7d1f3f3d174524ea0c540
Yuriy Tumakha <tumakha@gmail.com>
2019-11-23 23:18:32 +0000

    LRUCache

---
+++ src/main/scala/LRUCache.scala
@@ -0,0 +1,19 @@
+
+import java.util.Collections.synchronizedMap
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable
+
+/**
+ * @author Yuriy Tumakha
+ */
+class LRUCache[K, V](maxEntries: Int)
+  extends java.util.LinkedHashMap[K, V](100, .75f, true) {
+
+  override def removeEldestEntry(eldest: java.util.Map.Entry[K, V]): Boolean = size > maxEntries
+
+}
+
+object LRUCache {
+  def apply[K, V](maxEntries: Int): mutable.Map[K, V] = synchronizedMap(new LRUCache[K, V](maxEntries))
+}
---
+++ src/test/scala/LRUCacheSpec.scala
@@ -0,0 +1,19 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+ * @author Yuriy Tumakha
+ */
+class LRUCacheSpec extends FlatSpec with Matchers {
+
+  "LRUCache" should "work as Scala mutable Map" in {
+    val cache = LRUCache[String, String](1000)
+    val key = "key1"
+    val value = "value1"
+
+    cache.get(key) shouldBe None
+    cache += key -> value
+    cache.get(key) shouldBe Some(value)
+    cache(key) shouldBe value
+  }
+
+}
